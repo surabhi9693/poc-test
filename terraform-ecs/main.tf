82dd2b12e196a06ab2d65b267f4633ef9adc4c69
surabhi9693 <127684006+surabhi9693@users.noreply.github.com>
2024-06-08 23:35:58 +0530

    Update main.tf

--- terraform-ecs/main.tf
+++ terraform-ecs/main.tf
@@ -14,7 +14,7 @@ variable "docker_image" {
 
 
 resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
+  name = "ecsTaskExecutionRole-1"
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
